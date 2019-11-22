0d1b1ad718bbafcacac501447511e932852de208
Samuel Kim <ubuntor2000@gmail.com>
2019-11-21 19:27:10 -0500

    app typecheck

--- check/check.ml
+++ check/check.ml
@@ -21,23 +21,32 @@ let rec infer expr ~context : Ty.t Or_error.t =
     if Ty.equal t ty then return ()
     else Or_error.error_s [%message error (ty : Ty.t)]
   in
+  let delta, gamma = context in
   match expr with
   | Expr.Var x -> (
-      let delta, gamma = context in
       match Map.find delta x with
       | Some ty -> Ok ty
       | None -> (
           match Map.find gamma x with
           | Some ty -> Ok ty
           | None ->
-              Or_error.error_s [%message "variable not in context" (x : string)]
-          ) )
+              Or_error.error_s [%message "var: not in context" (x : string)] ) )
   | Fun _ -> Or_error.error_s [%message "not implemented"] (* TODO *)
   | Z -> Ok Nat
   | S0 e | S1 e ->
-      let%bind _ = assert_ty e ~ty:Nat ~error:"successor of non-nat" ~context in
+      let%bind _ = assert_ty e ~ty:Nat ~error:"successor: non-nat" ~context in
       Ok Ty.Nat
-  | App _ -> Or_error.error_s [%message "not implemented"] (* TODO *)
+  | App (e1, e2) -> (
+      let%bind arr_ty = infer e1 ~context in
+      let%bind ty1' = infer e2 ~context in
+      match arr_ty with
+      | Arr (ty1, ty2) ->
+          (* TODO subtyping? *)
+          if Ty.equal ty1 ty1' then Ok ty2
+          else
+            Or_error.error_s
+              [%message "app: type mismatch" (ty1 : Ty.t) (ty1' : Ty.t)]
+      | _ -> Or_error.error_s [%message "app: non-fun" (arr_ty : Ty.t)] )
   | Case (e, (e0, (x1, e1), (x2, e2))) ->
       let%bind _ = assert_ty e ~ty:Nat ~error:"case: e not nat" ~context in
       let%bind _ = assert_ty e0 ~ty:Nat ~error:"case: e0 not nat" ~context in
@@ -51,7 +60,6 @@ let rec infer expr ~context : Ty.t Or_error.t =
       in
       Ok Ty.Nat
   | Rec (e, (e0, (x1, y1, e1), (x2, y2, e2))) ->
-      let delta, _ = context in
       let%bind _ =
         assert_ty e ~ty:Nat ~error:"rec: e not nat"
           ~context:(delta, empty_context)
